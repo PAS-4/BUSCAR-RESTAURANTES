2555767215e63cef878011643f523fae3b4ad4f9
Patrese <patrese.20@gmail.com>
2021-09-12 01:03:50 -0300

    Criação do "input"

--- buscar-restaurantes/src/pages/Home/index.jsx
+++ buscar-restaurantes/src/pages/Home/index.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import bomprato from '../../assets/bomprato.png';
 
-import { Container } from './styled';
+import { Container, Busca} from './styled';
 
-const Home = () => <Container>Hello World</Container>
+const Home = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    return(
+        <Container>
+            <Busca>
+                <img src={bomprato} alt="logo do restaurante" />
+                <TextField label="Pesquisar">
+                    <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)}></Input>
+                </TextField>
+            </Busca>
+        </Container>
+    );
+}
 
 export default Home;
--- buscar-restaurantes/src/pages/Home/styled.js
+++ buscar-restaurantes/src/pages/Home/styled.js
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    // background-color: ${(props) => props.theme.colors.success};
-    // font-family: ${(props) => props.theme.fonts.regular};
+    background-color: ${(props) => props.theme.colors.background};
+    width: 360px;
+    height: 100vh;
+    overflow-y: auto;
+`;
+
+export const Busca = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: #fff;
 `;
